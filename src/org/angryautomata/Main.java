4c6a6e072415cdfc650be2c10e7b9c603b9c27ff
ZZZZZZ <amaurin.alexandre@yahoo.fr>
2016-06-06 15:39:37 +0200

    J'aime Githube

--- src/org/angryautomata/Main.java
+++ src/org/angryautomata/Main.java
@@ -9,6 +9,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		// sac a viande
 		Position origin = new Position(0, 0);
 		int[][] transitions = {{1, 0, 3, 2}, {2, 2, 0, 1}, {1, 0, 0, 1}, {0, 3, 3, 2}};
 		int[][] actions = {{1, 0, 3, 2}, {2, 2, 0, 1}, {1, 0, 0, 1}, {0, 3, 3, 2}, {0, 1, 2, 3}};
