486f30bbd04a05638517cae10391ddee141362d6
Ceciile <cathymarschul9317@gmail.com>
2016-06-06 17:03:46 +0200

    ReadXML(pas encore tester )

--- src/org/angryautomata/xml/XMLReader.java
+++ src/org/angryautomata/xml/XMLReader.java
@@ -43,18 +43,23 @@ public class XMLReader
                //process child element 
             } 
          } * */
-
-			NodeList list = doc.getElementsByTagName("disk");
+                        int sym = doc.getElementsByTagName("nb_symbole_max");
+			NodeList list = doc.getElementsByTagName("automate");
 
 			//ici, on attend des Attributes dans AUTOMATE.遍历该集合，显示结合中的元素及其子元素的名字
 			for(int i = 0; i < list.getLength(); i++)
 			{
 				Element element = (Element) list.item(i);
-				String autoname = element.getAttribute("name");
-				String etat = element.getElementsByTagName("etat").item(0).getFirstChild().getNodeValue();
-				String condition = element.getElementsByTagName("condition").item(0).getFirstChild().getNodeValue();
-				String action = element.getElementsByTagName("action").item(0).getFirstChild().getNodeValue();
-				String transition = element.getElementsByTagName("transition").item(0).getFirstChild().getNodeValue();
+				int autono = element.getAttribute("numero");
+				String joueur = element.getElementsByTagName("nom").item(0).getFirstChild().getNodeValue();
+				int etat = element.getElementsByTagName("nb_etat").item(0).getFirstChild().getNodeValue();
+				int[etat*sym][4] transition = null;
+				//String transition = element.getElementsByTagName("transition").item(0).getFirstChild().getNodeValue();
+				NodeList transitionList = element.getElementsByTagName("transition");  
+                                //if (transitionList == null) continue;
+                                for (int j = 0; j < transitionList.getLength(); j++) {  
+                                	transition[j] = transitionList.item(j);
+                                }  
 			}
 		}
 		catch(Exception e)
