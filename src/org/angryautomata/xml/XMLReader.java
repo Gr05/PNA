789d827ca47060d47dab70defe101f6ae556799e
Ceciile <cathymarschul9317@gmail.com>
2016-06-07 10:37:58 +0200

    Des Import pour xml->java

--- src/org/angryautomata/xml/XMLReader.java
+++ src/org/angryautomata/xml/XMLReader.java
@@ -1,11 +1,11 @@
 package org.angryautomata.xml;
 
+import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 //import java.util.*;
